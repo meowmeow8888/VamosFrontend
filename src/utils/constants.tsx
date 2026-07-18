2b84fa9caeaddda09c7feaa7cac1a3ed8530ccd7
meowmeow8888 <mosseriguy8@gmail.com>
2026-07-18 20:32:11 +0300

    fix: base url fix

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://7z7crp38-8000.uks1.devtunnels.ms";
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://7z7crp38-8000.uks1.devtunnels.ms";
+// export const BASE_URL = "http://localhost:8000";
