ed0fd4c444f7ee874cea7a0c7d443abae4658e6c
meowmeow8888 <mosseriguy8@gmail.com>
2026-07-18 18:59:11 +0300

    fix: ignore shuit

--- src/pages/Information.tsx
+++ src/pages/Information.tsx
@@ -1,10 +1,17 @@
 import MenuBar from "../components/menuBar";
 
 function Information() {
-  return <div className="w-screen h-screen">
-    <div className="">Hi Horesh</div>
-    <MenuBar></MenuBar>
-  </div>
+  return (
+    <div className="w-fit h-fit m-8">
+      <div className="flex flex-col h-full">
+        <textarea
+          className="flex-1 p-4 rounded-2xl border resize-none"
+          placeholder="Write here..."
+        />
+      </div>
+      <MenuBar></MenuBar>
+    </div>
+  );
 }
 
 export default Information;
