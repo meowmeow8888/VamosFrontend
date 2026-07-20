5b6deea21177e1ec73cefad30a43c7c462ff384d
meowmeow8888 <mosseriguy8@gmail.com>
2026-07-21 01:49:21 +0300

    chore: added lstorageable info page (no networking yet)

--- src/pages/Information.tsx
+++ src/pages/Information.tsx
@@ -1,13 +1,64 @@
+import { useEffect, useState } from "react";
 import MenuBar from "../components/menuBar";
+import { Pencil, Save } from "lucide-react";
 
 function Information() {
+  const [enableEdit, setEnableEdit] = useState(false);
+  const [editingIndex, setEditingIndex] = useState<number | null>(null);
+  const [lines, setLines] = useState<string[]>(() => {
+    const saved = localStorage.getItem("lines");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("lines", JSON.stringify(lines));
+  }, [lines]);
+
   return (
-    <div className="w-fit h-fit m-8">
-      <div className="flex flex-col h-full">
-        <textarea
-          className="flex-1 p-4 rounded-2xl border resize-none"
-          placeholder="Write here..."
+    <div
+      className={`h-fit m-8 p-2 rounded-2xl transition-all duration-300 ease-in-out ${
+        enableEdit ? "outline-2 bg-gray-700" : "bg-gray-500"
+      }`}
+    >
+      <div
+        className="size-10 absolute -translate-y-5 translate-x-80 z-50 rounded-2xl bg-white"
+        onClick={() => setEnableEdit((p) => !p)}
+      >
+        <Pencil
+          size={24}
+          className={`absolute translate-y-2 translate-x-2 transition-all text-black duration-300 ${
+            enableEdit ? "opacity-0 scale-80" : "opacity-100 scale-100"
+          }`}
         />
+
+        <Save
+          size={24}
+          className={`absolute translate-y-2 translate-x-2 transition-all text-black duration-300 ${
+            enableEdit ? "opacity-100 scale-100" : "opacity-0 scale-80"
+          }`}
+        />
+      </div>
+      <div className="flex flex-col h-full">
+        {lines.map((line, index) =>
+          enableEdit && index === editingIndex ? (
+            <input
+              className="w-full bg-transparent border-none outline-none p-0 m-0 font-inherit text-inherit"
+              key={index}
+              autoFocus
+              value={line}
+              onChange={(e) => {
+                const newLines = [...lines];
+                newLines[index] = e.target.value;
+                setLines(newLines);
+              }}
+              onBlur={() => setEditingIndex(null)}
+            />
+          ) : (
+            <p key={index} onClick={() => setEditingIndex(index)}>
+              {line || "\u00A0"}
+            </p>
+          ),
+        )}
       </div>
       <MenuBar></MenuBar>
     </div>
